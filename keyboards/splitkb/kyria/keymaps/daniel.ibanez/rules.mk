a8539ff297966535378e86bf42cd835e0872a6f9
Daniel Ibáñez <daniel.ibanez@geoactio.com>
2024-11-26 12:20:39 +0100

    Added custom layout

---
+++ keyboards/splitkb/kyria/keymaps/daniel.ibanez/rules.mk
@@ -0,0 +1,15 @@
+OLED_ENABLE = yes
+# OLED_DRIVER = SSD1306      # Enables the use of OLED displays. Is it needed?
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+
+# Reducing firmware size
+LTO_ENABLE = yes		   # Link time optimization (gets smaller compiled size)
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+MUSIC_ENABLE = no
+
+# Split keyboard rules
+SPLIT_KEYBOARD = yes
